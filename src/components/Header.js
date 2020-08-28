d5aa5e53112a63c77b5ab8acf49cec935e6cbd03
yashmunjal <yashmu45@gmail.com>
2020-08-28 23:56:56 +0530

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/384px-Facebook_icon.svg.png" alt="logo" />
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAO1pR9gMQjRZpGzsWHecgJNiWcJJRuQ_Y",
+    authDomain: "fb-clone-ba326.firebaseapp.com",
+    databaseURL: "https://fb-clone-ba326.firebaseio.com",
+    projectId: "fb-clone-ba326",
+    storageBucket: "fb-clone-ba326.appspot.com",
+    messagingSenderId: "18464311358",
+    appId: "1:18464311358:web:2f7b0ab907617ab9e1a96f",
+    measurementId: "G-7FMZ78G5RG"
+  };
